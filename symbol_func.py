e3b999ac728fb68eaef8a16627581e039c23921c
MikeDown <mikdowns@outlook.com>
2022-10-07 07:38:48 -0400

    Repo Cleanup

--- symbol_func.py
+++ symbol_func.py
@@ -1,3 +1,4 @@
+
 def stock_symbol():
     import pandas as pd
 
@@ -5,17 +6,25 @@ def stock_symbol():
 
     symbol = input("Please enter one of the following symbols!" + str(symbols) + '\n').upper()
 
-    if symbol in symbols:
-        pass
-    else:
-        print("You have entered an invalid symbol!")
+    try:
+        df = pd.read_csv('/Users/miked/Downloads/HistoricalData_' + symbol + '.csv')
+        df['Symbol'] = symbol
+        df['Open'] = df['Open'].str.replace("$", '', regex=True).astype(float)
+        df['High'] = df['High'].str.replace("$", '', regex=True).astype(float)
+        df['Low'] = df['Low'].str.replace("$", '', regex=True).astype(float)
+        df['Close/Last'] = df['Close/Last'].str.replace("$", '', regex=True).astype(float)
+        df.rename(columns={'Close/Last': 'Close'}, inplace=True)
+        df['Date']= pd.to_datetime(df['Date'])
+        print(df)
+        df2 = df['Open'].mean()
+        print(df2)
+    
+        return df
+        
+    except FileNotFoundError:
+        print("You have entered an invalid symbol! Please try again.")
         stock_symbol()
-    df = pd.read_csv('/Users/miked/Downloads/HistoricalData_' + symbol + '.csv')
-    df['Symbol'] = symbol
-    df['Open'] = df['Open'].str.replace("$", '', regex=True).astype(float)
-    df['High'] = df['High'].str.replace("$", '', regex=True).astype(float)
-    df['Low'] = df['Low'].str.replace("$", '', regex=True).astype(float)
-    df['Close/Last'] = df['Close/Last'].str.replace("$", '', regex=True).astype(float)
-    df.rename(columns={'Close/Last': 'Close'}, inplace=True)
-    #df.to_csv('/Users/miked/documents/Code_Louisville_Project/' + symbol + '.csv', index=False)
-    print(df)
+    
+    
+    
+
